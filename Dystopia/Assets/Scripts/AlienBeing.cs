db415b032c40f1a7cc2e6a6efda1dfc0b95b31c7
BroYee <qqas1211yj@gmail.com>
2019-03-11 01:25:04 +0900

    Init

---
+++ Dystopia/Assets/Scripts/AlienBeing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienBeing : MonoBehaviour
+{
+    public int crateMat;
+    public int crateSize;
+
+    public int species;
+    public float weight;
+    public float height;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dystopia/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private bool operating;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (operating)
+        {
+            Operate();
+        }
+
+    }
+
+    private void Operate()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
+
